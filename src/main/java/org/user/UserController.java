68e95d2643ecbacbc49f7a0bd1b3191e079266a2
JosephJostar0 <bohanxiong56@gmail.com>
2022-02-06 22:30:52 +0800

    新增注册

--- src/main/java/org/user/UserController.java
+++ src/main/java/org/user/UserController.java
@@ -10,6 +10,8 @@ import com.jfinal.kit.StrKit;
 import org.common.interceptor.CorsInterceptor;
 import org.common.module.User;
 
+import java.util.Objects;
+
 /**
  * 具体用户接参响应方法
  * @author Administrator
@@ -31,15 +33,35 @@ public class UserController extends Controller {
         User user = userService.getByUserNo(getPara("user_no"));
         if(!StrKit.notNull(user)){
             renderJson(BaseResult.fail("用户不存在"));
-            return;
         }
-        if(!getPara("password").equals(user.getPassword())){
+        else if(!getPara("password").equals(user.getPassword())){
             renderJson(BaseResult.fail("密码错误"));
-            return;
         }
         else{
             renderJson(BaseResult.ok("用户登录成功"));
-            return;
+        }
+    }
+
+    @Param(name = "user_no",required = true)
+    @Param(name = "school_no",required = true)
+    @Param(name = "name",required = true)
+    @Param(name = "password",required = true)
+    @Param(name = "phone",required = true)
+    @Param(name = "sex",required = true)
+    public void register(){
+        User user = userService.getByUserTel(getPara("phone"));
+        if(StrKit.notNull(user)){
+            renderJson(BaseResult.fail("该手机号已被注册！"));
+        }
+        else{
+            user = new User();
+            user.setUserNo(getPara("user_no"));
+            user.setSchoolNo(getPara("school_no"));
+            user.setName(getPara("name"));
+            user.setPassword(getPara("password"));
+            user.setPhone(getPara("phone"));
+            user.setSex("男".equals(getPara("sex")) ?0:1);
+            renderJson(user.save()?BaseResult.ok("注册成功！"):BaseResult.fail("注册失败"));
         }
     }
 }
--- src/main/java/org/user/UserService.java
+++ src/main/java/org/user/UserService.java
@@ -4,8 +4,6 @@ import cn.fabrice.jfinal.service.BaseService;
 import com.jfinal.kit.Kv;
 import org.common.module.User;
 
-import java.util.regex.Pattern;
-
 /**
  * 用户基础功能
  * @author Administrator
@@ -19,5 +17,10 @@ public class UserService  extends BaseService<User> {
     public User getByUserNo(String no){
         Kv cond = Kv.by("user_no",no);
         return get(cond,"getByUserNo");
+    }
+
+    public User getByUserTel(String tel){
+        Kv cond = Kv.by("phone",tel);
+        return get(cond,"getByUserTel");
     }
 }
